ff4e8681fdc78d6d3de36699f82fe44720c65f1b
alexa <alexa@DESKTOP-MM8O6ED>
2021-03-04 18:41:24 +0200

    classes modified

---
+++ seminar2/src/ro/ase/cts/program/Program.java
@@ -0,0 +1,22 @@
+package ro.ase.cts.program;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.readers.AngajatReader;
+public class Program {
+
+	public static void main(String[] args) {
+		List<Angajat> listaAngajati;
+		try {
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
+			for(Angajat angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
