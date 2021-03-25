84b9d5c3b086c2bcbcaba5ea15f37453a9ef3b7c
alexandragavril99 <alexandragavril19@gmail.com>
2021-03-25 18:10:05 +0200

    Simple factory

---
+++ seminar5/src/ro/ase/cts/main/Main.java
@@ -0,0 +1,22 @@
+package ro.ase.cts.main;
+
+import ro.ase.cts.clase.FactoryPersonal;
+import ro.ase.cts.clase.PersonalSpital;
+import ro.ase.cts.clase.TipPersonal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		FactoryPersonal factoryPersonal = new FactoryPersonal();
+		
+		PersonalSpital asistent = factoryPersonal.createPersonal(TipPersonal.Asistent, "David");
+		PersonalSpital medic = factoryPersonal.createPersonal(TipPersonal.Medic, "Razvan");
+
+		System.out.println(asistent);
+		System.out.println(medic);
+		
+	}
+
+}
