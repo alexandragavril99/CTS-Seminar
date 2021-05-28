187dff9c12827a0156e5e6d624192970bff7edb3
alexandragavril99 <alexandragavril19@gmail.com>
2021-05-28 15:29:43 +0300

    Final commit

--- seminar13/src/ro/ase/cts/teste/TesteGrupa.java
+++ seminar13/src/ro/ase/cts/teste/TesteGrupa.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.*;
 
 import java.time.Duration;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.IStudent;
+import ro.ase.cts.clase.Student;
 
 public class TesteGrupa {
-
+	
 	@Test
 	public void testConstructorNrGrupaCorect() {
 		Grupa grupa = new Grupa(1081);
@@ -56,4 +59,63 @@ public class TesteGrupa {
 //		assertTimeout(Duration.ofMillis(500), () -> {new Grupa(1081);});;
 //	}
 	
+	@Test
+	public void testGetPromovabilitateRight() {
+		Grupa grupa = new Grupa(1081);
+		for(int i=0; i<10; i++) {
+			IStudent student = new Student();
+			student.adaugaNota(5);
+			student.adaugaNota(7);
+			grupa.adaugaStudent(student);
+		}
+		for(int i=0; i<5; i++) {
+			IStudent student = new Student();
+			student.adaugaNota(4);
+			student.adaugaNota(7);
+			grupa.adaugaStudent(student);
+		}
+		
+		assertEquals(0.66f, grupa.getPromovabilitate(), 0.1);
+	}
+	
+	@Test
+	public void testGetPromovabilitateLimitataInferioare() {
+		Grupa grupa = new Grupa(1001);
+		for(int i=0; i<5; i++) {
+			IStudent student = new Student();
+			student.adaugaNota(4);
+			student.adaugaNota(7);
+			grupa.adaugaStudent(student);
+		}
+		
+		assertEquals(0, grupa.getPromovabilitate(),0.01);
+	}
+	
+	@Test
+	public void testGetPromovabilitateLimitaSuperioara() {
+		Grupa grupa = new Grupa(1081);
+		for(int i=0; i<10; i++) {
+			IStudent student = new Student();
+			student.adaugaNota(6);
+			student.adaugaNota(7);
+			grupa.adaugaStudent(student);
+		}
+		assertEquals(1, grupa.getPromovabilitate(), 0.01);
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetPromovabilitateErrorCondition() {
+		Grupa grupa = new Grupa(1050);
+		grupa.getPromovabilitate();
+	}
+	
+	@Test
+	public void testGetPromovabilitateCardinality1() {
+		Grupa grupa = new Grupa(1081);
+		IStudent stud = new Student();
+		stud.adaugaNota(5);
+		stud.adaugaNota(6);
+		grupa.adaugaStudent(stud);
+		assertEquals(1, grupa.getPromovabilitate(), 0.01);
+	}
 }
